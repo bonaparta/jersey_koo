554af8dbcad205830651af4185027adc26fc5c58
bonaparta <mingtsun17@gmail.com>
2016-08-09 15:28:58 +0800

    First Commit for Jersey Test

---
+++ jerseyTest/src/main/java/jerseyTest/MyApplication.java
@@ -0,0 +1,11 @@
+package jerseyTest;
+
+import javax.ws.rs.ApplicationPath;
+import org.glassfish.jersey.server.ResourceConfig;
+ 
+@ApplicationPath("rest")
+public class MyApplication extends ResourceConfig{
+    public MyApplication(){
+        packages("jerseyTest");
+    }
+}
